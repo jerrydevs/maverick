c57b00e9dcb30108e22f835165c1a3f22f6a6fa0
Jerry Zhang <jerryzhang.dev@gmail.com>
2020-12-13 21:43:47 -0500

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'maverick'
+import 'maverick/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
